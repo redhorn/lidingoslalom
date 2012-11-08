79f93e66377ab8cc4bccef43ab5bd58cd277288a
Joel <joel@redhorn.se>
2012-11-08 17:34:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lidingoslalom::Application.config.secret_token = 'd94243206a03704adac98bcd675baf8ffbbf4a21f2a877bde21d7b4243576cdc60436541c53a3041e98ab8a87233d4822848d2ee4922ebc8fc2647f2eb16b846'
